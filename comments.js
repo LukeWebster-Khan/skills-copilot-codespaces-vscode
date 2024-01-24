d601af18e557b2a2ada007dfb376f896bcf3773b
Luke_Webster_Khan <lukewebsterkhan@gmail.com>
2024-01-24 17:10:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,74 +1,39 @@
-// create web server
+// Create web server
+// 1. Create a web server
+// 2. Create a route for comments
+// 3. Create a route for comments/new
+// 4. Create a route for comments/:id
+// 5. Create a route for comments/:id/edit
+// 6. Create a route for comments/:id/delete
 
-var express = require('express');
-var router = express.Router();
+// 1. Create a web server
+const express = require('express');
+const router = express.Router();
 
-// import model
-var Comment = require('../models/Comment');
-
-// create route
-router.get('/', function(req, res, next) {
-    Comment.find({}, function(err, comments) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send(comments);
-        }
-    });
+// 2. Create a route for comments
+router.get('/', (req, res) => {
+    res.send('INDEX /comments');
 });
 
-router.get('/:id', function(req, res, next) {
-    Comment.findById(req.params.id, function(err, comment) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send(comment);
-        }
-    });
+// 3. Create a route for comments/new
+router.get('/new', (req, res) => {
+    res.send('NEW /comments/new');
 });
 
-router.post('/', function(req, res, next) {
-    var newComment = new Comment({
-        name: req.body.name,
-        comment: req.body.comment
-    });
-
-    newComment.save(function(err, comment) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send(comment);
-        }
-    });
+// 4. Create a route for comments/:id
+router.get('/:id', (req, res) => {
+    res.send('SHOW /comments/:id');
 });
 
-router.put('/:id', function(req, res, next) {
-    Comment.findById(req.params.id, function(err, comment) {
-        if (err) {
-            res.send('Error');
-        } else {
-            comment.name = req.body.name;
-            comment.comment = req.body.comment;
-            comment.save(function(err, comment) {
-                if (err) {
-                    res.send('Error');
-                } else {
-                    res.send(comment);
-                }
-            });
-        }
-    });
+// 5. Create a route for comments/:id/edit
+router.get('/:id/edit', (req, res) => {
+    res.send('EDIT /comments/:id/edit');
 });
 
-router.delete('/:id', function(req, res, next) {
-    Comment.findByIdAndRemove(req.params.id, function(err, comment) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send(comment);
-        }
-    });
+// 6. Create a route for comments/:id/delete
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /comments/:id');
 });
 
-// export router
+// 7. Export router
 module.exports = router;
